04ffa3fdb53f70612c276ac47cc6bd75c95b7ae7
phonguet <40883370+phonguet@users.noreply.github.com>
2019-11-24 21:22:14 +0700

    Add files via upload

---
+++ Bai3/covert.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+
+def toGray(rgba_im):
+    r, g, b = rgba_im[:,:,0], rgba_im[:,:,1], rgba_im[:,:,2]
+    gray = rgba_im.copy()
+    gray[:,:,0] = 0.3*r + 0.59*g + 0.11*b
+    gray[:,:,1] = 0.3*r + 0.59*g + 0.11*b
+    gray[:,:,2] = 0.3*r + 0.59*g + 0.11*b
+    try:
+        gray[:,:,3] = 1
+    except:
+    return 255*gray
+
+def toRed(rgba_im):
+    red = rgba_im.copy()
+    red[:,:,1] = 0
+    red[:,:,2] = 0
+    try:
+        red[:,:,3] = 1
+    except:
+    return 255*red
+    
+def toGreen(rgba_im):
+    green = rgba_im.copy()
+    green[:,:,0] = 0
+    green[:,:,2] = 0
+    try:
+        green[:,:,3] = 1
+    except:
+    return 255*green
+    
+def toBlue(rgba_im):
+    blue = rgba_im.copy()
+    blue[:,:,0] = 0
+    blue[:,:,1] = 0  
+    try:
+        blue[:,:,3] = 1
+    except:
+    return 255*blue
+    
+def toAlpha(rgba_im):
+    alpha = rgba_im.copy()
+    alpha[:,:,0] = 0
+    alpha[:,:,1] = 0
+    alpha[:,:,2] = 0
+    return 255*alpha
+    
+def recover(rscale_im, gscale_im, bscale_im, ascale_im):
+    rgba = rscale_im.copy()
+    rgba[:,:,1] = gscale_im[:,:,1]
+    rgba[:,:,2] = bscale_im[:,:,2]
+    try:
+        rgba[:,:,3] = ascale_im[:,:,3]
+    except:
+    return 255*rgba
+
+rgba_im = plt.imread('D:\Kỳ 1 năm 4\Truyền thông đa phương tiện\BTLT-Nhom6\Bai3\convert_test.png', 0)
+
+gray_img = toGray(rgba_im).astype('uint8')
+imageio.imwrite('gray.png', gray_img)
+
+red_img = toRed(rgba_im).astype('uint8')
+imageio.imwrite('red.png', red_img)
+
+green_img = toGreen(rgba_im).astype('uint8')
+imageio.imwrite('green.png', green_img)
+
+blue_img = toBlue(rgba_im).astype('uint8')
+imageio.imwrite('blue.png', blue_img)
+
+alpha_img = toAlpha(rgba_im).astype('uint8')
+imageio.imwrite('alpha.png', alpha_img)
